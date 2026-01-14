1408a1e5e9a52c92ba27ec7c24afbee2a41c248c
Wesley Junio <wesjrpiri@gmail.com>
2026-01-14 21:13:23 +0000

    first commit

---
+++ src/main/java/com/example/bappoint_backend/BappointBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bappoint_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BappointBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BappointBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bappoint_backend/BappointBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bappoint_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BappointBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
