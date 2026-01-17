0aaf4c8bcaac3ecbb95a7497fa0acdeff28a2598
Wesley Junio <wesjrpiri@gmail.com>
2026-01-17 18:35:44 +0000

    feat: add and configure docker and postgres for testing.

--- src/main/java/com/wesleysilva/bappoint/BappointBackendApplication.java
+++ src/main/java/com/wesleysilva/bappoint/BappointBackendApplication.java
@@ -2,17 +2,12 @@ package com.wesleysilva.bappoint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class,
-		DataSourceTransactionManagerAutoConfiguration.class
-})
+@SpringBootApplication
 public class BappointBackendApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(BappointBackendApplication.class, args);
 	}
+
 }
