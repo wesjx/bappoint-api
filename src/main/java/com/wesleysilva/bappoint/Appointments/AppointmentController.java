3204351613277a9571662c38bbcdb532a90aadc8
Wesley Junio <wesjrpiri@gmail.com>
2026-02-03 12:41:50 +0000

    feat: inject SettingsService to get appointment interval to declare into the method to calcule the moinutes.

--- src/main/java/com/wesleysilva/bappoint/Appointments/AppointmentController.java
+++ src/main/java/com/wesleysilva/bappoint/Appointments/AppointmentController.java
@@ -1,12 +1,10 @@
 package com.wesleysilva.bappoint.Appointments;
 
 import com.wesleysilva.bappoint.Appointments.SlotTimes.SlotTimesDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("company/{companyId}/appointments")
@@ -19,7 +17,7 @@ public class AppointmentController {
     }
 
     @GetMapping("/available-times")
-    public List<SlotTimesDTO> getAvailableTimes(@RequestParam String date) {
-        return appointmentService.findAvailableSlots(date);
+    public List<SlotTimesDTO> getAvailableTimes(@PathVariable UUID companyId, @RequestParam String date) {
+        return appointmentService.findAvailableSlots(companyId, date);
     }
 }
--- src/main/java/com/wesleysilva/bappoint/Appointments/AppointmentService.java
+++ src/main/java/com/wesleysilva/bappoint/Appointments/AppointmentService.java
@@ -5,6 +5,9 @@ import com.wesleysilva.bappoint.Company.CompanyRepository;
 import com.wesleysilva.bappoint.OperatingHours.OperatingHoursModel;
 import com.wesleysilva.bappoint.OperatingHours.OperatingHoursRepository;
 import com.wesleysilva.bappoint.OperatingHours.OperatingHoursService;
+import com.wesleysilva.bappoint.Settings.SettingsDTO;
+import com.wesleysilva.bappoint.Settings.SettingsService;
+import com.wesleysilva.bappoint.enums.AppointmentInterval;
 import com.wesleysilva.bappoint.enums.WeekDay;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class AppointmentService {
@@ -20,16 +24,21 @@ public class AppointmentService {
     private final CompanyRepository companyRepository;
     private final OperatingHoursService operatingHoursService;
     private final OperatingHoursRepository operatingHoursRepository;
+    private final SettingsService settingsService;
 
-    public AppointmentService(AppointmentRepository appointmentRepository, CompanyRepository companyRepository, OperatingHoursService operatingHoursService, OperatingHoursRepository operatingHoursRepository) {
+    public AppointmentService(AppointmentRepository appointmentRepository, CompanyRepository companyRepository, OperatingHoursService operatingHoursService, OperatingHoursRepository operatingHoursRepository, SettingsService settingsService) {
         this.appointmentRepository = appointmentRepository;
         this.companyRepository = companyRepository;
         this.operatingHoursService = operatingHoursService;
         this.operatingHoursRepository = operatingHoursRepository;
+        this.settingsService = settingsService;
     }
 
-    public List<SlotTimesDTO> findAvailableSlots(String dateParams) {
+    public List<SlotTimesDTO> findAvailableSlots(UUID companyId, String dateParams) {
 
+        SettingsDTO settings = settingsService.getByCompanyId(companyId);
+
+        AppointmentInterval appointmentInterval = settings.getAppointment_interval();
         LocalDate date = LocalDate.parse(dateParams);
         DayOfWeek dayOfWeek = date.getDayOfWeek();
         WeekDay weekday = WeekDay.valueOf(dayOfWeek.name());
@@ -50,7 +59,7 @@ public class AppointmentService {
         LocalDateTime current = from;
 
         while (current.isBefore(to)) {
-            LocalDateTime next = current.plusMinutes(30);
+            LocalDateTime next = current.plusMinutes(appointmentInterval.getMinutes());
             slots.add(new SlotTimesDTO(current.toString(), next.toString()));
             current = next;
         }
