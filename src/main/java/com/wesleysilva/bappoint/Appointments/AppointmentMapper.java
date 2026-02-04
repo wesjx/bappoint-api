d105fdfeabecad8d111a1edf9c1381541e58f4be
Wesley Junio <wesjrpiri@gmail.com>
2026-02-04 15:40:18 +0000

    feat: create and add into the AppointmentMapper dto for Appointment response.

--- src/main/java/com/wesleysilva/bappoint/Appointments/AppointmentMapper.java
+++ src/main/java/com/wesleysilva/bappoint/Appointments/AppointmentMapper.java
@@ -1,6 +1,10 @@
 package com.wesleysilva.bappoint.Appointments;
 
+import com.wesleysilva.bappoint.Services.ServiceModel;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
 
 @Component
 public class AppointmentMapper {
@@ -10,9 +14,7 @@ public class AppointmentMapper {
         appointmentModel.setCostumerName(appointmentsDTO.getCostumerName());
         appointmentModel.setCostumerEmail(appointmentsDTO.getCostumerEmail());
         appointmentModel.setCostumerPhone(appointmentsDTO.getCostumerPhone());
-        appointmentModel.setAppointmentDate(appointmentsDTO.getAppointmentDate());
         appointmentModel.setStartTime(appointmentsDTO.getStartTime());
-        appointmentModel.setEndTime(appointmentsDTO.getEndTime());
         appointmentModel.setAppointmentStatus(appointmentsDTO.getAppointmentStatus());
 
         return appointmentModel;
@@ -24,11 +26,31 @@ public class AppointmentMapper {
         appointmentsDTO.setCostumerName(appointmentModel.getCostumerName());
         appointmentsDTO.setCostumerEmail(appointmentModel.getCostumerEmail());
         appointmentsDTO.setCostumerPhone(appointmentModel.getCostumerPhone());
-        appointmentsDTO.setAppointmentDate(appointmentModel.getAppointmentDate());
         appointmentsDTO.setStartTime(appointmentModel.getStartTime());
-        appointmentsDTO.setEndTime(appointmentModel.getEndTime());
         appointmentsDTO.setAppointmentStatus(appointmentModel.getAppointmentStatus());
 
         return appointmentsDTO;
+    }
+
+    public AppointmentResponseDTO toResponseDTO(AppointmentModel appointment) {
+        AppointmentResponseDTO dto = new AppointmentResponseDTO();
+        dto.setId(appointment.getId());
+        dto.setCostumerName(appointment.getCostumerName());
+        dto.setCostumerEmail(appointment.getCostumerEmail());
+        dto.setCostumerPhone(appointment.getCostumerPhone());
+        dto.setAppointmentDate(appointment.getAppointmentDate());
+        dto.setStartTime(appointment.getStartTime());
+        dto.setEndTime(appointment.getEndTime());
+        dto.setTotalAmount(appointment.getTotalAmount());
+        dto.setAppointmentStatus(appointment.getAppointmentStatus());
+
+        dto.setServiceIds(Optional.ofNullable(appointment.getServices())
+                .map(services -> services.stream()
+                        .map(ServiceModel::getId)
+                        .toList())
+                .orElse(List.of()));
+
+        dto.setCompanyId(appointment.getCompany().getId());
+        return dto;
     }
 }
