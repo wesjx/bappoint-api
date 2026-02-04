cb8189619b1c9147e56a652392863fdee555bbd2
Wesley Junio <wesjrpiri@gmail.com>
2026-02-04 15:41:24 +0000

    fix update type of variables startTime and endTime fot LocalDateTime.

--- src/main/java/com/wesleysilva/bappoint/Appointments/AppointmentModel.java
+++ src/main/java/com/wesleysilva/bappoint/Appointments/AppointmentModel.java
@@ -1,6 +1,7 @@
 package com.wesleysilva.bappoint.Appointments;
 
 import com.wesleysilva.bappoint.Company.CompanyModel;
+import com.wesleysilva.bappoint.Services.ServiceModel;
 import com.wesleysilva.bappoint.Settings.SettingsModel;
 import com.wesleysilva.bappoint.enums.AppointmentStatus;
 import jakarta.persistence.*;
@@ -8,13 +9,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Data
-@Table(name = "appointment")
+@Table(name = "appointments")
 @AllArgsConstructor
 @NoArgsConstructor
 public class AppointmentModel {
@@ -28,16 +31,24 @@ public class AppointmentModel {
     private String costumerEmail;
     private String costumerPhone;
 
-    private LocalDateTime appointmentDate;
+    private LocalDate appointmentDate;
 
-    private long startTime;
-    private long endTime;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
 
     private double totalAmount;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "appointment_status")
     private AppointmentStatus appointmentStatus;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "appointment_services",
+            joinColumns = @JoinColumn(name = "appointment_id"),
+            inverseJoinColumns = @JoinColumn(name = "service_id")
+    )
+    private List<ServiceModel> services;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "company_id")
