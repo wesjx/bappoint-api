b48c758713a6d5e9adf150c67ed4407f9d9fb4ee
Wesley Junio <wesjrpiri@gmail.com>
2026-02-06 14:00:55 +0000

    feat: create routes for crud for appointments.

--- src/main/java/com/wesleysilva/bappoint/Appointments/AppointmentController.java
+++ src/main/java/com/wesleysilva/bappoint/Appointments/AppointmentController.java
@@ -53,10 +53,43 @@ public class AppointmentController {
             summary = "",
             description = ""
     )
-    public ResponseEntity<List<AppointmentResponseDTO>> listAppointments(@PathVariable UUID companyId) {
-        List<AppointmentResponseDTO> appointments = appointmentService.findByCompanyId(companyId).stream()
-                .map(appointmentMapper::toResponseDTO)
-                .collect(Collectors.toList());
+    public ResponseEntity<List<AppointmentResponseDTO>> listAppointments() {
+        List<AppointmentResponseDTO> appointments = appointmentService.listAppointments();
+
         return ResponseEntity.status(HttpStatus.OK).body(appointments);
+    }
+
+    @GetMapping("/{appointmentId}")
+    @Operation(
+            summary = "",
+            description = ""
+    )
+    public ResponseEntity<AppointmentResponseDTO> listAppointmentById(@PathVariable UUID appointmentId) {
+        AppointmentResponseDTO appointment = appointmentService.getAppointmentById(appointmentId);
+        return ResponseEntity.ok(appointment);
+    }
+
+    @PutMapping("/update/{appointmentId}")
+    @Operation(
+            summary = "",
+            description = ""
+    )
+    public ResponseEntity<?> updateAppointment(@RequestBody AppointmentResponseDTO appointmentResponseDTO, @PathVariable UUID appointmentId) {
+        AppointmentResponseDTO appointment = appointmentService.updateAppointment(appointmentId, appointmentResponseDTO);
+
+        if (appointment != null) {
+            return ResponseEntity.ok(appointment);
+        } else  {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("delete/{appointmentId}")
+    @Operation(
+            summary = "",
+            description = ""
+    )
+    void deleteAppointment(UUID appointmentId) {
+        appointmentService.deleteAppointment(appointmentId);
     }
 }
