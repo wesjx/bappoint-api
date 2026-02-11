8bbbb8b63f291a6847adb7511b05b8f8ca89a95e
Wesley Junio <wesjrpiri@gmail.com>
2026-02-11 22:03:54 +0000

    feat(company): create dto separate for update company.

--- src/main/java/com/wesleysilva/bappoint/Company/dto/UpdateCompanyDTO.java
+++ src/main/java/com/wesleysilva/bappoint/Company/dto/UpdateCompanyDTO.java
@@ -1,4 +1,33 @@
 package com.wesleysilva.bappoint.Company.dto;
 
-public class UpdateComapnyDTO {
+import com.wesleysilva.bappoint.Appointments.AppointmentDTO;
+import com.wesleysilva.bappoint.Settings.SettingsDTO;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateCompanyDTO {
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String name;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotEmpty
+    private String phone;
+
+    @NotEmpty
+    private String address;
+
+    @Valid
+    @NotNull
+    private SettingsDTO settings;
 }
