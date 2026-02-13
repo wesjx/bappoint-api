7f164b0a4e22e21fa169e7afadcc05b175eb9b50
Wesley Junio <wesjrpiri@gmail.com>
2026-02-13 17:53:22 +0000

    fix: change the method name for existsByEmail.

--- src/main/java/com/wesleysilva/bappoint/Company/CompanyRepository.java
+++ src/main/java/com/wesleysilva/bappoint/Company/CompanyRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface CompanyRepository extends JpaRepository<CompanyModel, UUID> {
-    boolean existsEmail(String email);
+    boolean existsByEmail(String email);
 }
