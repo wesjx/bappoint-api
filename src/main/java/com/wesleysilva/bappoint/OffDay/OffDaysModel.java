1fd71298f7fe8a96f9243194c6d47a4de61839d3
Wesley Junio <wesjrpiri@gmail.com>
2026-02-17 18:30:17 +0000

    refactor: add new annotations and named each atributes.

--- src/main/java/com/wesleysilva/bappoint/OffDay/OffDaysModel.java
+++ src/main/java/com/wesleysilva/bappoint/OffDay/OffDaysModel.java
@@ -15,14 +15,17 @@ public class OffDaysModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "off_days_id")
     private UUID id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "settings_id")
+    @JoinColumn(name = "settings_id", nullable = false)
     private SettingsModel settings;
 
+    @Column(name = "reason")
     private String reason;
 
+    @Column(name = "date", nullable = false)
     private LocalDate date;
 
     @Enumerated(EnumType.STRING)
