612ff6057bb807241aabe38004194b7e289e59e0
Wesley Junio <wesjrpiri@gmail.com>
2026-02-17 18:32:13 +0000

    feat: create dto for off days response.

--- src/main/java/com/wesleysilva/bappoint/OffDay/dto/OffDaysResponseDTO.java
+++ src/main/java/com/wesleysilva/bappoint/OffDay/dto/OffDaysResponseDTO.java
@@ -1,4 +1,24 @@
 package com.wesleysilva.bappoint.OffDay.dto;
 
-public class OffDaysResponse {
+import com.wesleysilva.bappoint.enums.OffDaysType;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class OffDaysResponseDTO {
+    @NotBlank
+    @Size(min = 3, max = 500)
+    private String reason;
+
+    @NotNull
+    @FutureOrPresent
+    private LocalDate date;
+
+    @NotNull
+    private OffDaysType offDaysType;
 }
