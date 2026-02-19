7561671f70d28e55da275369bc77c9b6f154c20f
Wesley Junio <wesjrpiri@gmail.com>
2026-02-19 14:52:52 +0000

    feat: create new dto for update in operating hours.

--- src/main/java/com/wesleysilva/bappoint/OperatingHours/dto/UpdateOperatingHoursDTO.java
+++ src/main/java/com/wesleysilva/bappoint/OperatingHours/dto/UpdateOperatingHoursDTO.java
@@ -1,4 +1,33 @@
 package com.wesleysilva.bappoint.OperatingHours.dto;
 
+import com.wesleysilva.bappoint.enums.WeekDay;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.time.LocalTime;
+
+@Data
 public class UpdateOperatingHoursDTO {
+
+    @Enumerated(EnumType.STRING)
+    @NotBlank
+    @Size(min = 1, max = 10)
+    private WeekDay weekday;
+
+    @NotNull
+    private Boolean isActive;
+
+    @NotNull
+    private LocalTime startTime;
+
+    @NotNull
+    private LocalTime endTime;
+
+    private LocalTime lunchStartTime;
+
+    private LocalTime lunchEndTime;
 }
