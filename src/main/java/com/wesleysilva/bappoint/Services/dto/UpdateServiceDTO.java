41c7e86fb064556d21dfaba3296988c6379bedcf
Wesley Junio <wesjrpiri@gmail.com>
2026-02-20 10:33:53 +0000

    feat: create new dto to update service.

--- src/main/java/com/wesleysilva/bappoint/Services/dto/UpdateServiceDTO.java
+++ src/main/java/com/wesleysilva/bappoint/Services/dto/UpdateServiceDTO.java
@@ -1,4 +1,25 @@
 package com.wesleysilva.bappoint.Services.dto;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class UpdateServiceDTO {
+    @NotNull
+    private String name;
+
+    @NotNull
+    @DecimalMin(value = "0.0", inclusive = false)
+    @DecimalMax(value = "99999.99")
+    private BigDecimal price;
+
+    @NotNull
+    private Integer durationMinutes;
+
+    @NotNull
+    private Boolean isActive;
 }
