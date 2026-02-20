2c4a66573512bab6a4c3cf6007b27d5268aedd6f
Wesley Junio <wesjrpiri@gmail.com>
2026-02-20 10:28:54 +0000

    feat: create methods for new dtos.

--- src/main/java/com/wesleysilva/bappoint/Services/ServiceMapper.java
+++ src/main/java/com/wesleysilva/bappoint/Services/ServiceMapper.java
@@ -1,27 +1,62 @@
 package com.wesleysilva.bappoint.Services;
 
+import com.wesleysilva.bappoint.Services.dto.CreateServiceDTO;
+import com.wesleysilva.bappoint.Services.dto.ServiceAllDetailsDTO;
+import com.wesleysilva.bappoint.Services.dto.ServiceResponseDTO;
+import com.wesleysilva.bappoint.Services.dto.UpdateServiceDTO;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ServiceMapper {
 
-    public ServiceModel toEntity(ServiceDTO serviceDTO) {
+    public ServiceModel toEntity(CreateServiceDTO serviceDTO) {
         ServiceModel serviceModel = new ServiceModel();
         serviceModel.setId(serviceDTO.getId());
         serviceModel.setName(serviceDTO.getName());
         serviceModel.setPrice(serviceDTO.getPrice());
-        serviceModel.setDuration_minutes(serviceDTO.getDuration_minutes());
-        serviceModel.setIs_active(serviceDTO.getIs_active());
+        serviceModel.setDurationMinutes(serviceDTO.getDurationMinutes());
+        serviceModel.setIsActive(serviceDTO.getIsActive());
         return serviceModel;
     }
 
-    public ServiceDTO toDto(ServiceModel serviceModel) {
-        ServiceDTO serviceDTO = new ServiceDTO();
+    public CreateServiceDTO toCreate(ServiceModel serviceModel) {
+        CreateServiceDTO serviceDTO = new CreateServiceDTO();
         serviceDTO.setId(serviceModel.getId());
         serviceDTO.setName(serviceModel.getName());
         serviceDTO.setPrice(serviceModel.getPrice());
-        serviceDTO.setDuration_minutes(serviceModel.getDuration_minutes());
-        serviceDTO.setIs_active(serviceModel.getIs_active());
+        serviceDTO.setDurationMinutes(serviceModel.getDurationMinutes());
+        serviceDTO.setIsActive(serviceModel.getIsActive());
+        return serviceDTO;
+    }
+
+    public ServiceResponseDTO toResponse(ServiceModel serviceModel) {
+        ServiceResponseDTO serviceDTO = new ServiceResponseDTO();
+
+        serviceDTO.setName(serviceModel.getName());
+        serviceDTO.setPrice(serviceModel.getPrice());
+        serviceDTO.setDurationMinutes(serviceModel.getDurationMinutes());
+        serviceDTO.setIsActive(serviceModel.getIsActive());
+
+        return serviceDTO;
+    }
+
+    public ServiceAllDetailsDTO toResponseAllDetails(ServiceModel serviceModel) {
+        ServiceAllDetailsDTO serviceDTO = new ServiceAllDetailsDTO();
+        serviceDTO.setId(serviceModel.getId());
+        serviceDTO.setName(serviceModel.getName());
+        serviceDTO.setPrice(serviceModel.getPrice());
+        serviceDTO.setDurationMinutes(serviceModel.getDurationMinutes());
+        serviceDTO.setIsActive(serviceModel.getIsActive());
+        return serviceDTO;
+    }
+
+    public UpdateServiceDTO toUpdate(ServiceModel serviceModel) {
+        UpdateServiceDTO serviceDTO = new UpdateServiceDTO();
+
+        serviceDTO.setName(serviceModel.getName());
+        serviceDTO.setPrice(serviceModel.getPrice());
+        serviceDTO.setDurationMinutes(serviceModel.getDurationMinutes());
+        serviceDTO.setIsActive(serviceModel.getIsActive());
         return serviceDTO;
     }
 }
