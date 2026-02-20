3c0a32d669fcfd5e38346388f720911577aacca2
Wesley Junio <wesjrpiri@gmail.com>
2026-02-20 12:25:41 +0000

    feat: create new dto to response of settings.

--- src/main/java/com/wesleysilva/bappoint/Settings/dto/SettingsResponseDTO.java
+++ src/main/java/com/wesleysilva/bappoint/Settings/dto/SettingsResponseDTO.java
@@ -1,4 +1,18 @@
 package com.wesleysilva.bappoint.Settings.dto;
 
+import com.wesleysilva.bappoint.enums.AppointmentInterval;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
 public class SettingsResponseDTO {
+    private UUID id;
+
+    @NotNull
+    private AppointmentInterval appointmentInterval;
+
+    @NotNull
+    private Integer maxCancellationInterval;
 }
