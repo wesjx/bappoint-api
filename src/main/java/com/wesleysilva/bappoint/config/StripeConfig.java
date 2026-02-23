a237aa1c510b1fa9afd5ab086b1da1bfa1c7b003
Wesley Junio <wesjrpiri@gmail.com>
2026-02-23 23:21:57 +0000

    feat: create config for stripe connect.

--- src/main/java/com/wesleysilva/bappoint/config/StripeConfig.java
+++ src/main/java/com/wesleysilva/bappoint/config/StripeConfig.java
@@ -1,4 +1,18 @@
 package com.wesleysilva.bappoint.config;
 
+import com.stripe.Stripe;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import jakarta.annotation.PostConstruct;
+
+@Configuration
 public class StripeConfig {
+
+    @Value("${stripe.secret.key}")
+    private String secretKey;
+
+    @PostConstruct
+    public void init() {
+        Stripe.apiKey = secretKey;
+    }
 }
