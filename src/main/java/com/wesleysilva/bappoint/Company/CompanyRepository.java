0e0359a27f2fa1d6df6226c7e611966fff71a614
Wesley Junio <wesjrpiri@gmail.com>
2026-02-26 17:53:22 +0000

    feat: create method to find company by clerk id.

--- src/main/java/com/wesleysilva/bappoint/Company/CompanyRepository.java
+++ src/main/java/com/wesleysilva/bappoint/Company/CompanyRepository.java
@@ -2,8 +2,10 @@ package com.wesleysilva.bappoint.Company;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CompanyRepository extends JpaRepository<CompanyModel, UUID> {
     boolean existsByEmail(String email);
+    Optional<CompanyModel> findByClerkUserId(String clerkUserId);
 }
