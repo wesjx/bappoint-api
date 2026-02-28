c5212e394f6b211bd3c5c9e8c4ecc42aea8b69b9
Wesley Junio <wesjrpiri@gmail.com>
2026-02-28 18:06:29 +0000

    feat: create response and request dtos for stripe checkout.

--- src/main/java/com/wesleysilva/bappoint/stripe/dto/PaymentRequestDTO.java
+++ src/main/java/com/wesleysilva/bappoint/stripe/dto/PaymentRequestDTO.java
@@ -1,8 +1,15 @@
-package com.wesleysilva.bappoint.Appointments.dto;
+package com.wesleysilva.bappoint.stripe.dto;
 
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
+@AllArgsConstructor
 public class PaymentRequestDTO {
 
+    @NotNull
+    private UUID appointmentId;
 }
