fb81c985a3131e139bdba8c4a9a07803afcb6b15
Wesley Junio <wesjrpiri@gmail.com>
2026-03-03 18:30:13 +0000

    feat: create Stripe web hook controller.

--- src/main/java/com/wesleysilva/bappoint/stripe/StripeWebhookController.java
+++ src/main/java/com/wesleysilva/bappoint/stripe/StripeWebhookController.java
@@ -1,4 +1,48 @@
 package com.wesleysilva.bappoint.stripe;
 
-public class StripeWebHookController {
+import com.stripe.model.Event;
+import com.stripe.net.Webhook;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/webhooks/stripe")
+public class StripeWebhookController {
+
+    @Value("${stripe.webhook.secret}")
+    private String endpointSecret;
+
+    private final StripeService stripeService;
+
+    public StripeWebhookController(StripeService stripeService) {
+        this.stripeService = stripeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> handleWebhook(
+            @RequestBody String payload,
+            @RequestHeader("Stripe-Signature") String sigHeader
+    ) {
+
+        Event event;
+
+        try {
+            event = Webhook.constructEvent(
+                    payload,
+                    sigHeader,
+                    endpointSecret
+            );
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Invalid signature");
+        }
+
+        if ("checkout.session.completed".equals(event.getType())) {
+            stripeService.handleCheckoutCompleted(event);
+        }
+
+        return ResponseEntity.ok("success");
+    }
 }
