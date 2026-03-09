a6d091321f1e7039477cc6a5556542bd38ec084d
Wesley Junio <wesjrpiri@gmail.com>
2026-03-09 20:27:41 +0000

    feat: injection CORS configuration in.

--- src/main/java/com/wesleysilva/bappoint/clerk/SecurityConfig.java
+++ src/main/java/com/wesleysilva/bappoint/clerk/SecurityConfig.java
@@ -4,24 +4,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     private final ClerkJwtFilter clerkJwtFilter;
+    private final CorsConfigurationSource corsConfigurationSource;
 
-    public SecurityConfig(ClerkJwtFilter clerkJwtFilter) {
+    public SecurityConfig(ClerkJwtFilter clerkJwtFilter,
+                          CorsConfigurationSource corsConfigurationSource) {
         this.clerkJwtFilter = clerkJwtFilter;
+        this.corsConfigurationSource = corsConfigurationSource;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
