f3b233ea0bd7062efadbe001232fcd560ef0bab8
Wesley Junio <wesjrpiri@gmail.com>
2026-03-09 20:24:54 +0000

    feat: create CORS for security of responses.

--- src/main/java/com/wesleysilva/bappoint/infra/CorsConfig.java
+++ src/main/java/com/wesleysilva/bappoint/infra/CorsConfig.java
@@ -1,4 +1,29 @@
 package com.wesleysilva.bappoint.infra;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
 public class CorsConfig {
+    @Value("app.cors.allowed-origins")
+    private String allowedOrigins;
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(allowedOrigins.split(",")));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
 }
