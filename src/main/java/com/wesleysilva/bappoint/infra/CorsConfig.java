b63408963319c9af97ff102c2e7132190f1de1e3
Wesley Junio <wesjrpiri@gmail.com>
2026-07-21 19:42:53 +0100

    fix: add allowed method 'PATCH' into the cors config.

--- src/main/java/com/wesleysilva/bappoint/infra/CorsConfig.java
+++ src/main/java/com/wesleysilva/bappoint/infra/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(allowedOrigins.split(",")));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
         config.setAllowedOrigins(List.of(
