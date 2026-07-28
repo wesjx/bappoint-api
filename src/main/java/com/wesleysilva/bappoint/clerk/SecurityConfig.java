9eab2fd283a83746e83ab0a62484cf03b0eb06b1
Wesley Junio <wesjrpiri@gmail.com>
2026-07-28 20:48:31 +0100

    fix: fix options.

--- src/main/java/com/wesleysilva/bappoint/clerk/SecurityConfig.java
+++ src/main/java/com/wesleysilva/bappoint/clerk/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.wesleysilva.bappoint.ratelimit.PublicEndpointRateLimitFilter;
 import com.wesleysilva.bappoint.ratelimit.RateLimiterService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -38,6 +39,7 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
